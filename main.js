aca3b11d4451948a2e3e68a9511c7ff4fd0e34c3
marcreativo <marcreativo@gmail.com>
2019-06-25 20:17:46 -0300

    students instance

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 let allStudents = []
 
+function student(first_name, last_name, birthdate){
+  this.first_name = first_name
+  this.last_name = last_name
+  this.birthdate = birthdate
+  this.greet = () => console.log(`Hola, soy ${this.first_name}`)
+}
+
 const createStudent = () => {
   let firstnameField = document.getElementById('first_name')
   let lastnameField = document.getElementById('last_name')
   let birthdateField = document.getElementById('birthdate')
-  let student = {
-    first_name: firstnameField.value,
-    last_name: lastnameField.value,
-    birthdate: birthdateField.value
-  }
-  allStudents.push(student)
+  let newStudent = new student(firstnameField.value, lastnameField.value, birthdate.value)
+  allStudents.push(newStudent)
 }
