ea60a8968972743b7f6807eb89350c98a91765e0
marcreativo <marcreativo@gmail.com>
2019-06-27 19:48:39 -0300

    birthdate

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ let allStudents = []
 function student(first_name, last_name, birthdate){
   this.first_name = first_name
   this.last_name = last_name
-  this.birthdate = birthdate
+  this.birthdate = new Date(birthdate)
   this.greet = () => console.log(`Hola, soy ${this.first_name}`)
+  this.timeToParty = () => {
+    let currentMonth = new Date().getMonth()
+    console.log( currentMonth - this.birthdate.getMonth())
+  }
 }
 
 const createStudent = () => {
